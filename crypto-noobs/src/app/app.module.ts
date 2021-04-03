36721a536b590959d68029ebabb5222581d8b099
Thomas <Thomas@Thomass-Mac-Pro.local>
2021-04-03 15:57:26 -0700

    added firebase waiting for initial merge

--- crypto-noobs/src/app/app.module.ts
+++ crypto-noobs/src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule, routingComponents } from '../app/app-routing.module';
+import { environment } from 'src/environments/environment.prod';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
 
 import { AppComponent } from './app.component';
+import { FirestoreService } from './services/firebase.service';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    routingComponents
   ],
   imports: [
-    BrowserModule
+    AppRoutingModule,
+    BrowserModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    AngularFireModule.initializeApp(environment.firebase, 'crypto-noobs'),
+    AngularFireDatabaseModule,
+    AngularFirestoreModule,
   ],
-  providers: [],
+  providers: [FirestoreService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
