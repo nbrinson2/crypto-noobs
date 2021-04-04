edc49b92ca3bbf6f0ecf5abae58eebc3c704a1b6
Thomas <Thomas@Thomass-Mac-Pro.local>
2021-04-03 17:34:22 -0700

    added user-join-model

---
+++ crypto-noobs/src/app/models/user-join-form.ts
@@ -0,0 +1,10 @@
+export interface SubmitFormModel {
+    userName: string;
+    password: string;
+    firstName: string;
+    lastName: string;
+    email: string;
+    phone: number;
+    isValid: boolean;
+    id?: string;
+  }
