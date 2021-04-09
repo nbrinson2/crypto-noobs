07383ba031a7ab1b1555ea989dff9ce5e4421b73
ewsams <edwardsams@email.arizona.edu>
2021-04-08 18:56:59 -0700

    contact page added

--- crypto-noobs/src/app/pages/miscellaneous/contact-us/contact-us.component.ts
+++ crypto-noobs/src/app/pages/miscellaneous/contact-us/contact-us.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'ngx-contact-us',
   templateUrl: './contact-us.component.html',
-  styleUrls: ['./contact-us.component.scss']
+  styleUrls: ['./contact-us.component.scss'],
 })
 export class ContactUsComponent implements OnInit {
 
